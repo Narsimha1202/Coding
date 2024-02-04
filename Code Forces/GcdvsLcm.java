b5020c99ecaaef926c6fe2a5fabd681d73d1b975
G Narsimha Reddy <gundarapunarsimhareddy@gmail.com>
2024-02-04 19:17:39 +0530

    Create GcdvsLcm.java

---
+++ Code Forces/GcdvsLcm.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+ 
+public class LcmGcf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            System.out.print(n - 3 + " " + 1 + " " + 1 + " " + 1);
+            System.out.println();
+        }
+    }
+}
